5ac2850ce0ece5e575afa57e2df2d1121974b7f2
Valter Iversen Fernandes de Souza <iversen.souza@gmail.com>
2023-02-18 12:08:57 -0300

    First Step e Second Step atualizado

--- components/register_firststep.js
+++ components/register_firststep.js
@@ -1,11 +1,18 @@
 
 "use client"
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Image from 'next/image';
 import { motion, AnimatePresence } from "framer-motion";
 
 import styles from '../styles/register.module.css'
 
 export default function FirstStep({ nextStep }) {
+
+    const onClick = () => {
+        const data = {}
+        nextStep(data);
+    }
+
     return (
         <AnimatePresence>
             <motion.div
@@ -16,7 +23,30 @@ export default function FirstStep({ nextStep }) {
                 exit={{ opacity: 0, y: -25 }}
                 >
                 <div className={styles.cardContent}>
-                    <h3>Antes de tudo, gostariamos de saber quem é você</h3>
+                    <h3 className={styles.darkerFont}>Antes de tudo, gostariamos de saber quem é você</h3>
+                    <form className={styles.form}>
+                        <div>
+                            <label for="name" className={styles.inputLabel}>Nome:</label>
+                            <input type="text" id="name" name="name" 
+                                maxLength={70}
+                                className={styles.input}/>
+                        </div>
+                        <div>
+                            <label for="phone" className={styles.inputLabel}>Celular:</label>
+                            <input type="text" id="phone" name="phone" 
+                                maxLength={15}
+                                className={styles.input}/>
+                        </div>
+                        <a className={styles.nextLink} onClick={onClick}>Prazer em te conhecer! Qual data?
+                            <Image
+                                alt="próximo passo"
+                                className={styles.nextIcon}
+                                src="/next.png"
+                                width={19}
+                                height={19}
+                                />
+                        </a>
+                    </form>
                 </div>
             </motion.div>
         </AnimatePresence>
